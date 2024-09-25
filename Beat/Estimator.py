cdd24acb8a9f10d53da01f2261ae3f435ebc7f77
Leon <158475552+Leon-JS02@users.noreply.github.com>
2024-09-25 14:27:02 +0100

    Update Estimator.py
    Fixed docstrings.

--- Beat/Estimator.py
+++ Beat/Estimator.py
@@ -2,14 +2,14 @@ import numpy as np
 
 class Estimator:
 
-    """Reduces the likelihood of the estimation being half or double-time tempo"""
     def normalise_bpm(self, bpm):
+        """Reduces the likelihood of the estimation being half or double-time tempo"""
         if bpm > 160: bpm /= 2
         elif bpm < 60: bpm *= 2
         return round(bpm)
         
-    """Naively calculates a BPM estimate, given an array of onset times"""
     def naive_estimate_bpm(self, onset_times):
+        """Naively calculates a BPM estimate, given an array of onset times"""
         # Calculate inter-onset intervals
         IoIs = np.diff(onset_times)
         # Calculate average IoI
@@ -18,8 +18,9 @@ class Estimator:
         estimated_bpm = round(60/mean_IoI)
         return self.normalise_bpm(estimated_bpm)
     
-    """Calculates a BPM estimate based on the most common inter-onset-interval, given an array of onset times"""
     def interval_based_estimate_bpm(self, onset_times):
+        """Calculates a BPM estimate based on the most common inter-onset-interval, 
+        given an array of onset times"""
         inter_onset_arrivals = np.diff(onset_times)
         inter_onset_arrivals = np.around(inter_onset_arrivals, 3)
         occurrences = dict()
